a63d342c264d03e298c77b9edcbbbe6a1898d79b
Orlando Marin <orlandojermainemarin@gmail.com>
2021-02-22 23:50:12 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  if (distance > 2500) {
+    return "No can do.";
+  }
+  else if (distance > 2000) {
+    return "I will gladly take your thirty bucks.";
+  }
+  else if (distance <= 400) {
+    return "This one is on me!";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName) {
+  if (cityName === "NYC") {
+    return "Ok, sounds good.";
+  } else return "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmt) {
+  if (tipAmt === "generous") {
+    return "Thank you so much.";
+  } 
+  else if (tipAmt === "not as generous") {
+    return "Thank you.";
+  } 
+  else return "Bye.";
 }
